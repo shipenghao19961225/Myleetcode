eee89beaaae4d1ad6f79bbea90bed87f4f1fad68
Penghao Shi <penghao.shi@ericsson.com>
2020-11-16 17:41:26 +0800

    406

--- leetcode/406. queue-reconstruction-by-height.go
+++ leetcode/406. queue-reconstruction-by-height.go
@@ -3,14 +3,13 @@ package leetcode
 import "sort"
 
 func reconstructQueue(people [][]int) [][]int {
-	res := make([][]int, 0, len(people))
 	sort.Slice(people, func(i, j int) bool {
 		a, b := people[i], people[j]
 		return a[0] > b[0] || a[0] == b[0] && a[1] < b[1]
 	})
-	for _, person := range people {
-		index := person[1]
-		res = append(res[:index], append([][]int{person}, res[index:]...)...)
+	for i, p := range people {
+		copy(people[p[1]+1:i+1], people[p[1]:i])
+		people[p[1]] = p
 	}
-	return res
+	return people
 }
