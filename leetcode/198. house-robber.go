71ef24596f8d6399516b04c72a8959b607479d5c
shipenghao19961225 <shipenghao19961225@hotmail.com>
2020-12-15 22:38:30 +0800

    dp

--- leetcode/198. house-robber.go
+++ leetcode/198. house-robber.go
@@ -20,3 +20,30 @@ func max(a, b int) int {
 	}
 	return b
 }
+
+// 另一种解法，覃超老师说这种二维三维的动态规划往往更加常用
+func rob(nums []int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	if len(nums) == 1 {
+		return nums[0]
+	}
+	n := len(nums)
+	dp := make([][]int, n)
+	for i := 0; i < len(dp); i++ {
+		dp[i] = make([]int, 2)
+	}
+	dp[0][0], dp[0][1] = 0, nums[0]
+	for i := 1; i < n; i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1])
+		dp[i][1] = dp[i-1][0] + nums[i]
+	}
+	return max(dp[n-1][0], dp[n-1][1])
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
--- leetcode/70. climbing-stairs.go
+++ leetcode/70. climbing-stairs.go
@@ -36,3 +36,51 @@ func climbStairs(n int) int {
 	}
 	return q
 }
+
+// 变种
+// 每次可以爬1到m级台阶
+
+func climbStairs2(n, m int) int {
+	if n <= 1 {
+		return n
+	}
+	dp := make([]int, n+1)
+	dp[0], dp[1] = 1, 1
+	for i := 2; i <= n; i++ {
+		for j := 1; j <= m && i-j >= 0; j++ {
+			dp[i] += dp[i-j]
+		}
+	}
+	return dp[n]
+}
+
+// 每次可以爬1到m级台阶，但是相邻之间的步数不能相同，这里还是需要通过填表的方法，解决动态规划问题
+// 只不过步骤较多，较为繁琐
+
+func climbStairs3(n, m int) int {
+	if n <= 1 {
+		return n
+	}
+	dp := make([][]int, n+1)
+	for i := 0; i < n+1; i++ {
+		dp[i] = make([]int, m+1)
+	}
+	for i := 1; i <= n; i++ {
+		for j := 1; j <= m && i >= j; j++ {
+			if i == j {
+				dp[i][j] = 1
+			}
+			for k := 1; k <= m; k++ {
+				if k == j {
+					continue
+				}
+				dp[i][j] += dp[i-j][k]
+			}
+		}
+	}
+	res := 0
+	for _, v := range dp[n] {
+		res += v
+	}
+	return res
+}
